efbd5b682dc367b78b864895d76dd922bfe66939
Henrique-Linhares <hlinhares453@gmail.com>
2025-08-25 19:13:28 -0300

    Alterações no componente

--- src/components/CardDefeitos.js
+++ src/components/CardDefeitos.js
@@ -5,17 +5,24 @@ import "./CardDefeitos.css";
 export default function CardDefeitos({ defeito }) {
   const [contador, setContador] = useState(0);
 
+  // Cria a função que atualiza o contador
   const handleClick = () => {
     setContador(contador + 1);
   };
 
   let classeContador = "contador-verde";
 
+  // Verifica se o contador é maior que 7 e menor que 15
+  // e atribui uma cor amarela ao contador
   if (contador >= 7 && contador <= 15) {
     classeContador = "contador-amarelo";
+  //Verifica se o contador é maior que 15 e atribui uma cor vermelha a ele
   } else if (contador > 15) {
     classeContador = "contador-vermelho";
   }
+
+  // Se o contador estiver acima de 0 o texto será escondido
+  const visibilidade = contador > 0 ? "ocultarParagrafo" : "pDefeito";
 
   return (
     <div className="card">
@@ -25,6 +32,7 @@ export default function CardDefeitos({ defeito }) {
       <button className="buttonAdd" onClick={handleClick}>
         Adicionar
       </button>
+      <p className={visibilidade}>Nenhum defeito registrado.</p>
     </div>
   );
 }
